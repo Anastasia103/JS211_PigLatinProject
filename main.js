e72dd4e9a6abc4d1a1dc47d03eb52bcdd8a88809
Anastasia103 <anastasia.sutherland12@gmail.com>
2022-07-14 23:35:55 -0400

    created graphical interface so now user can add words, need to add stylistic choices

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-'use strict';
+// 'use strict';
 
 // brings in the assert module for unit testing
-const assert = require('assert');
+//const assert = require('assert');
 // brings in the readline module to access the command line
-const readline = require('readline');
+//const readline = require('readline');
 // use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+//const rl = readline.createInterface({
+ // input: process.stdin,
+ // output: process.stdout
+//});
 // if the first letter is a vowel add "yay"
+// let piggeywords = ""
+// function updatePiggyWords() {
+//   let piggeywords = document.getElementById("piggywords").value
 
-
-const pigLatin = (word) => {
-// let vowelArr = ["a", "e", "i", "o", "u"]
-// let newWord = cleanword + "yay"
-// let secondletter =cleanword.slice(1,2)
-// let firstletter= cleanword.slice(0,1)
-// let newerWord = cleanword.substring(1) + firstletter +"ay"  
-// let newestWord = cleanword.substring(2) + cleanword.slice(0,2) +"ay"
-let groupOfWords = word.split(" ")
+//   console.log(piggeywords)
+// }
+const pigLatin = () => {  
+let bestpiggy= document.getElementById("piggywords").value;
+let groupOfWords= bestpiggy.split(" ")
+console.log(typeof bestpiggy)
+console.log(groupOfWords)
 let newlistwords = ""
 for (let value of groupOfWords) {
   let cleanwords = value.trim().toLowerCase()
@@ -41,6 +42,7 @@ else if (cleanwords= typeof(string)){
 
   // Your code here
 }
+document.getElementById("translatedWords").innerHTML = (newlistwords)
 console.log(newlistwords)
 }
 
@@ -48,41 +50,41 @@ console.log(newlistwords)
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
-const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    pigLatin(answer);
-    getPrompt();
-  });
-}
+//const getPrompt = () => {
+ // rl.question('word ', (answer) => {
+  //  pigLatin(answer);
+   // getPrompt();
+  //});
+//}
 
 // Unit Tests
 // to use them run the command: npm test main.js
 // to close them ctrl + C
-if (typeof describe === 'function') {
+//if (typeof describe === 'function') {
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
+  //describe('#pigLatin()', () => {
+    //it('should translate a simple word', () => {
+     // assert.equal(pigLatin('car'), 'arcay');
+//       assert.equal(pigLatin('dog'), 'ogday');
+//     });
+//     it('should translate a complex word', () => {
+//       assert.equal(pigLatin('create'), 'eatecray');
+//       assert.equal(pigLatin('valley'), 'alleyvay');
+//     });
+//     it('should attach "yay" if word begins with vowel', () => {
+//       assert.equal(pigLatin('egg'), 'eggyay');
+//       assert.equal(pigLatin('emission'), 'emissionyay');
+//     });
+//     it('should lowercase and trim word before translation', () => {
+//       assert.equal(pigLatin('HeLlO '), 'ellohay');
+//       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+//     });
+//   });
+// } else {
 
-  getPrompt();
+//   getPrompt();
 
-}
+// }
 
 
 
